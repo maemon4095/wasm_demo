b62839b8c36c3297ca439393c886719a1c10d25e
maemon4095 <maemondecocted@gmail.com>
2022-12-24 18:17:54 +0900

    version

---
+++ demo.js
@@ -0,0 +1,98 @@
+const resultContainer = document.getElementById("result");
+let binaryBuffer = null;
+
+(async () => {
+    const wabt = await WabtModule();
+    require.config({ paths: { 'vs': 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.34.1/min/vs' } })
+
+    window.MonacoEnvironment = {
+        getWorkerUrl: function (workerId, label) {
+            return `data:text/javascript;charset=utf-8,${encodeURIComponent(`
+        self.MonacoEnvironment = {
+          baseUrl: 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.34.1/min/'
+        };
+        importScripts('https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.34.1/min/vs/base/worker/workerMain.js');`
+            )}`
+        }
+    }
+
+    const localStorage = window.localStorage;
+    const jsKey = "js";
+    const watKey = "wat";
+    let jsEditor, watEditor, resultEditor;
+    require(['vs/editor/editor.main'], () => {
+        const options = {
+            automaticLayout: true,
+            theme: "vs-dark",
+        };
+
+        jsEditor = monaco.editor.create(document.getElementById('jscode'), {
+            value: localStorage.getItem(jsKey),
+            language: 'javascript',
+            ...options,
+        });
+
+        watEditor = monaco.editor.create(document.getElementById('watcode'), {
+            value: localStorage.getItem(watKey),
+            language: 'wat',
+            ...options,
+        });
+
+        resultEditor = monaco.editor.create(resultContainer, {
+            language: 'plaintext',
+            readOnly: true,
+            ...options,
+        });
+
+        jsEditor.addCommand(monaco.KeyMod.CtrlCmd | monaco.KeyCode.KeyS, () => {
+            jsEditor.getAction('editor.action.formatDocument').run();
+            const value = jsEditor.getValue();
+            localStorage.setItem(jsKey, value);
+            update();
+        });
+
+        watEditor.addCommand(monaco.KeyMod.CtrlCmd | monaco.KeyCode.KeyS, () => {
+            watEditor.getAction('editor.action.formatDocument').run();
+            const value = watEditor.getValue();
+            localStorage.setItem(watKey, value);
+            update();
+        });
+    });
+
+    const wrappedConsole = Object.create(console);
+    wrappedConsole.log = (...args) => {
+        const line = args.map(String).join('') + '\n';
+        resultEditor.setValue(resultEditor.getValue() + line);
+    };
+
+    function update() {
+        resultEditor.setValue('');
+        try {
+            evalWat(watEditor.getValue());
+            evalJs(jsEditor.getValue());
+        } catch (e) {
+            resultEditor.setValue(e.toString());
+        }
+    }
+
+    function evalWat(source) {
+        let module;
+        let features = undefined;
+        try {
+            module = wabt.parseWat('test.wast', source, features);
+            module.resolveNames();
+            module.validate(features);
+            const binaryOutput = module.toBinary({ log: true, write_debug_names: true });
+            binaryBuffer = binaryOutput.buffer;
+        } finally {
+            if (module) module.destroy();
+        }
+    }
+
+    function evalJs(source) {
+        if (binaryBuffer === null) return;
+        let wasm = new WebAssembly.Module(binaryBuffer);
+        const fn = new Function('wasmModule', 'console', source);
+        fn(wasm, wrappedConsole);
+    }
+})();
